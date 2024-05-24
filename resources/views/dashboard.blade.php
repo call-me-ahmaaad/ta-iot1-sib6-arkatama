daa553447ca21b008da7beb1894f2700cdcd838f
Muhammad <muhammadofc07@gmail.com>
2024-05-24 21:16:43 +0700

    Mengganti Link CSS di Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -22,7 +22,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/css/dashboard.css">
+    <link rel="stylesheet" href={{URL::asset("/css/dashboard.css")}}>
     <title>Document</title>
 </head>
 <body>

d19d125e0e4b0713b17a60023a7dc1758b27d437
Muhammad <muhammadofc07@gmail.com>
2024-05-27 21:59:19 +0700

    Ngubah Waktu Update

--- resources/views/dht11.blade.php
+++ resources/views/dht11.blade.php
@@ -155,7 +155,7 @@
                 }
 
                 // Fetch the latest temperature every second
-                setInterval(fetchLatestTemp_c, 1000);
+                setInterval(fetchLatestTemp_c, 5000);
             });
         </script>
     </div>

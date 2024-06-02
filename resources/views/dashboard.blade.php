e5f10f21054ca0b90037ff75880262697748401b
Muhammad <muhammadofc07@gmail.com>
2024-06-02 18:56:42 +0700

    kapan bisa dah

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -25,11 +25,7 @@
     <link rel="stylesheet" href={{URL::asset("/css/dashboard.css")}}>
     <title>Document</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://code.highcharts.com/highcharts.js"></script>
-    <script src="https://code.highcharts.com/modules/solid-gauge.js"></script>
-
 </head>
 <body>
     {{-- Bagian Title --}}
@@ -73,67 +69,15 @@
         <a class="button" id="gas" href={{route('web.mq2')}} id="gas">
             <h3>Gas</h3>
             <p><span id="gas_value">{{ $gas_value }}</span></p>
+            <div id="gasGaugeContainer" style="width: 400px; height: 300px;"></div>
             <div id="container-gas" style="width: 300px; height: 200px;"></div>
         </a>
 
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
         <script>
             $(document).ready(function() {
                 var tempValue;
                 var humidValue;
-
-                // Gauge Options
-const gaugeOptions = {
-    chart: {
-        type: 'solidgauge'
-    },
-    title: null,
-    pane: {
-        center: ['50%', '85%'],
-        size: '140%',
-        startAngle: -90,
-        endAngle: 90,
-        background: {
-            backgroundColor: Highcharts.defaultOptions.legend.backgroundColor || '#fafafa',
-            borderRadius: 5,
-            innerRadius: '60%',
-            outerRadius: '100%',
-            shape: 'arc'
-        }
-    },
-    exporting: {
-        enabled: false
-    },
-    tooltip: {
-        enabled: false
-    },
-    yAxis: {
-        stops: [
-            [0.1, '#55BF3B'], // green
-            [0.5, '#DDDF0D'], // yellow
-            [0.9, '#DF5353'] // red
-        ],
-        lineWidth: 0,
-        tickWidth: 0,
-        minorTickInterval: null,
-        tickAmount: 2,
-        title: {
-            y: -70
-        },
-        labels: {
-            y: 16
-        }
-    },
-    plotOptions: {
-        solidgauge: {
-            borderRadius: 3,
-            dataLabels: {
-                y: 5,
-                borderWidth: 0,
-                useHTML: true
-            }
-        }
-    }
-};
 
                 function fetchLatestTempAndHumid() {
                     $.ajax({
@@ -237,12 +181,6 @@ const gaugeOptions = {
                             var gasValue = data.gas_value;
                             $('#gas_value').text(gasValue + ' ppm');
 
-                            // Update the gas gauge
-                            if (chartGas) {
-                                var point = chartGas.series[0].points[0];
-                                point.update(gasValue);
-                            }
-
                             // Check if the gas value exceeds 1400
                             if (gasValue > 1400) {
                                 sendWhatsAppAlert(gasValue, tempValue, humidValue);
@@ -289,31 +227,6 @@ const gaugeOptions = {
                     }
                 }
 
-                // The gas gauge
-                const chartGas = Highcharts.chart('container-gas', Highcharts.merge(gaugeOptions, {
-                    yAxis: {
-                        min: 0,
-                        max: 2000,
-                        title: {
-                            text: 'Gas Value'
-                        }
-                    },
-                    series: [{
-                        name: 'Gas Value',
-                        data: [0],
-                        dataLabels: {
-                            format:
-                                '<div style="text-align:center">' +
-                                '<span style="font-size:25px">{y}</span><br/>' +
-                                '<span style="font-size:12px;opacity:0.4">ppm</span>' +
-                                '</div>'
-                        },
-                        tooltip: {
-                            valueSuffix: ' ppm'
-                        }
-                    }]
-                }));
-
                 // Fetch the latest temperature and humidity every 1 second
                 setInterval(fetchLatestTempAndHumid, 1000);
 
@@ -324,6 +237,113 @@ const gaugeOptions = {
                 setInterval(fetchLatestMq2, 1000);
             });
         </script>
+
+<script>
+    $(document).ready(function() {
+        // Gauge Options
+        const gaugeOptions = {
+            chart: {
+                type: 'solidgauge'
+            },
+            title: null,
+            pane: {
+                center: ['50%', '85%'],
+                size: '140%',
+                startAngle: -90,
+                endAngle: 90,
+                background: {
+                    backgroundColor: Highcharts.defaultOptions.legend.backgroundColor || '#fafafa',
+                    borderRadius: 5,
+                    innerRadius: '60%',
+                    outerRadius: '100%',
+                    shape: 'arc'
+                }
+            },
+            exporting: {
+                enabled: false
+            },
+            tooltip: {
+                enabled: false
+            },
+            yAxis: {
+                stops: [
+                    [0.1, '#55BF3B'], // green
+                    [0.5, '#DDDF0D'], // yellow
+                    [0.9, '#DF5353'] // red
+                ],
+                lineWidth: 0,
+                tickWidth: 0,
+                minorTickInterval: null,
+                tickAmount: 2,
+                title: {
+                    y: -70
+                },
+                labels: {
+                    y: 16
+                }
+            },
+            plotOptions: {
+                solidgauge: {
+                    borderRadius: 3,
+                    dataLabels: {
+                        y: 5,
+                        borderWidth: 0,
+                        useHTML: true
+                    }
+                }
+            }
+        };
+
+        // The gas gauge
+        const chartGas = Highcharts.chart('container-gas', Highcharts.merge(gaugeOptions, {
+            yAxis: {
+                min: 0,
+                max: 2000,
+                title: {
+                    text: 'Gas Value'
+                }
+            },
+            series: [{
+                name: 'Gas Value',
+                data: [0],
+                dataLabels: {
+                    format:
+                        '<div style="text-align:center">' +
+                        '<span style="font-size:25px">{y}</span><br/>' +
+                        '<span style="font-size:12px;opacity:0.4">ppm</span>' +
+                        '</div>'
+                },
+                tooltip: {
+                    valueSuffix: ' ppm'
+                }
+            }]
+        }));
+
+        // Function to fetch latest MQ2 data
+        function fetchLatestMq2() {
+            $.ajax({
+                url: '/latest-mq2',
+                method: 'GET',
+                success: function(data) {
+                    var gasValue = data.gas_value;
+                    $('#gas_value').text(gasValue + ' ppm');
+
+                    // Update the gas gauge
+                    if (chartGas) {
+                        var point = chartGas.series[0].points[0];
+                        point.update(gasValue);
+                    }
+                },
+                error: function(error) {
+                    console.log('Error fetching latest gas data:', error);
+                }
+            });
+        }
+
+        // Fetch the latest MQ2 data every 2 seconds
+        setInterval(fetchLatestMq2, 1000);
+    });
+    </script>
 
         {{-- LED Control --}}
         <div class="led" href="" id="led">

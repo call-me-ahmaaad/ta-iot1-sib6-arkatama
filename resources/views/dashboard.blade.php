bf0ce78d5f8b690914f11b09126904bc8f9d3606
Muhammad <muhammadofc07@gmail.com>
2024-06-03 02:43:57 +0700

    perbaikan di dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -75,8 +75,7 @@
             <div class="info">
                 <p><span id="gas_value">{{ $gas_value }}</span></p>
                 <div class="gasGauge">
-                    <div id="container-gauge" style="width: 200px; height: 200px;"></div>
-                    <p id="gas_value">Loading...</p>
+                    <div id="container-gauge" style="width: 120px; height: 120px;"></div>
                 </div>
             </div>
         </a>

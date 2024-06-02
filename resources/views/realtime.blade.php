7ed1a5f2d9091380ef76571bcfabe62b355b3b02
Muhammad <muhammadofc07@gmail.com>
2024-06-02 20:39:25 +0700

    cek realtime lagi

--- resources/views/realtime.blade.php
+++ resources/views/realtime.blade.php
@@ -6,11 +6,7 @@
     <title>Grafik DHT11 Real-Time</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://code.highcharts.com/highcharts.js"></script>
-    <script src="https://code.highcharts.com/highcharts-more.js"></script>
-    <script src="https://code.highcharts.com/modules/solid-gauge.js"></script>
-    <script src="https://code.highcharts.com/modules/exporting.js"></script>
-    <script src="https://code.highcharts.com/modules/export-data.js"></script>
-    <script src="https://code.highcharts.com/modules/accessibility.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 <body>
     <div>
@@ -22,113 +18,60 @@
         <div id="humidityChart"></div>
     </div>
 
-    <div id="container-gas" style="width: 300px; height: 200px;"></div>
+    <canvas id="gasGauge" width="400" height="400"></canvas>
 
     <script>
-        $(document).ready(function() {
-            // Function to fetch latest MQ2 data
-            function fetchLatestMq2() {
-                $.ajax({
-                    url: '/latest-mq2',
-                    method: 'GET',
-                    success: function(data) {
-                        var gasValue = data.gas_value;
-                        $('#gas_value').text(gasValue + ' ppm');
+        function fetchLatestMq2() {
+    $.ajax({
+        url: '/latest-mq2',
+        method: 'GET',
+        success: function(data) {
+            var gasValue = data.gas_value;
+            $('#gas_value').text(gasValue + ' ppm');
+            updateGauge(gasValue);
+        },
+        error: function(error) {
+            console.log('Error fetching latest gas data:', error);
+        }
+    });
+}
 
-                        // Update the gas gauge
-                        if (chartGas) {
-                            var point = chartGas.series[0].points[0];
-                            point.update(gasValue);
-                        }
-                    },
-                    error: function(error) {
-                        console.log('Error fetching latest gas data:', error);
-                    }
-                });
-            }
-
-            // Gauge Options
-            const gaugeOptions = {
-                chart: {
-                    type: 'solidgauge'
-                },
-                title: null,
-                pane: {
-                    center: ['50%', '85%'],
-                    size: '140%',
-                    startAngle: -90,
-                    endAngle: 90,
-                    background: {
-                        backgroundColor: Highcharts.defaultOptions.legend.backgroundColor || '#fafafa',
-                        borderRadius: 5,
-                        innerRadius: '60%',
-                        outerRadius: '100%',
-                        shape: 'arc'
-                    }
-                },
-                exporting: {
-                    enabled: false
-                },
-                tooltip: {
-                    enabled: false
-                },
-                yAxis: {
-                    stops: [
-                        [0.1, '#55BF3B'], // green
-                        [0.5, '#DDDF0D'], // yellow
-                        [0.9, '#DF5353'] // red
-                    ],
-                    lineWidth: 0,
-                    tickWidth: 0,
-                    minorTickInterval: null,
-                    tickAmount: 2,
-                    title: {
-                        y: -70
-                    },
-                    labels: {
-                        y: 16
-                    }
-                },
-                plotOptions: {
-                    solidgauge: {
-                        borderRadius: 3,
-                        dataLabels: {
-                            y: 5,
-                            borderWidth: 0,
-                            useHTML: true
-                        }
-                    }
-                }
-            };
-
-            // The gas gauge
-            const chartGas = Highcharts.chart('container-gas', Highcharts.merge(gaugeOptions, {
-                yAxis: {
-                    min: 0,
-                    max: 2000,
-                    title: {
-                        text: 'Gas Value'
-                    }
-                },
-                series: [{
-                    name: 'Gas Value',
-                    data: [0],
-                    dataLabels: {
-                        format:
-                            '<div style="text-align:center">' +
-                            '<span style="font-size:25px">{y}</span><br/>' +
-                            '<span style="font-size:12px;opacity:0.4">ppm</span>' +
-                            '</div>'
-                    },
-                    tooltip: {
-                        valueSuffix: ' ppm'
-                    }
+function updateGauge(gasValue) {
+    // Jika gauge belum ada, buat yang baru
+    if (!window.gasGauge) {
+        var ctx = document.getElementById('gasGauge').getContext('2d');
+        window.gasGauge = new Chart(ctx, {
+            type: 'doughnut',
+            data: {
+                labels: ['Gas Level'],
+                datasets: [{
+                    data: [gasValue, 1000 - gasValue],  // 1000 adalah nilai maksimum, bisa disesuaikan
+                    backgroundColor: ['#FF0000', '#EEEEEE'],
+                    borderWidth: 0
                 }]
-            }));
-
-            // Fetch the latest MQ2 data every 2 seconds
-            setInterval(fetchLatestMq2, 1000);
+            },
+            options: {
+                circumference: Math.PI,
+                rotation: Math.PI,
+                cutout: '70%',
+                plugins: {
+                    tooltip: { enabled: false },
+                    legend: { display: false }
+                }
+            }
         });
+    } else {
+        // Update gauge yang sudah ada
+        window.gasGauge.data.datasets[0].data[0] = gasValue;
+        window.gasGauge.data.datasets[0].data[1] = 1000 - gasValue;  // 1000 adalah nilai maksimum, bisa disesuaikan
+        window.gasGauge.update();
+    }
+}
+
+// Panggil fungsi fetchLatestMq2 untuk pertama kali dan set interval untuk memperbarui data
+fetchLatestMq2();
+setInterval(fetchLatestMq2, 5000);  // Memperbarui setiap 5 detik, bisa disesuaikan
+
         </script>
 
     <script>

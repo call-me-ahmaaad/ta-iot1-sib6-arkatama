4979e650ca58191c204963044aa65c44a85f2462
Muhammad <muhammadofc07@gmail.com>
2024-06-02 19:04:00 +0700

    .

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -27,6 +27,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/highcharts-more.js"></script>
+    <script src="https://code.highcharts.com/modules/solid-gauge.js"></script>
 </head>
 <body>
     {{-- Bagian Title --}}

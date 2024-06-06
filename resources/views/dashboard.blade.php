ac88c1e8901faddb2177f63820778a1fc6577308
Muhammad <muhammadofc07@gmail.com>
2024-06-06 07:33:29 +0700

    Perbaikan di dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -23,6 +23,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href={{URL::asset("/css/dashboard.css")}}>
+    <link rel="shortcut icon" href={{URL::asset("/image/favicon/monitoring.ico")}} type="image/x-icon">
     <title>Main Monitoring Page</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
